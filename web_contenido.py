213eb641cb712a4d8a7b2ee655d00521020aa636
OttoOctavius <OttoOctavius@users.noreply.github.com>
2018-03-29 22:01:07 -0300

    Tratando de buscar datos
    Peticion web para leer pagina oficial

---
+++ web_contenido.py
@@ -0,0 +1,45 @@
+from urllib import request
+import html.parser
+import re  # para expresiones regulares, texto
+
+# create a subclass and override the handler methods
+class MyHTMLParser(html.parser.HTMLParser):
+
+    def __init__(self):
+        super(MyHTMLParser,self).__init__()
+        self.resetflag()
+
+    def resetflag(self):
+        self.flag = False
+
+    def handle_starttag(self, tag, attrs):
+        print("Encountered a start tag:", tag)
+        if tag == 'p':
+            print("Encontrado el texto")
+            self.flag = True
+
+    def handle_endtag(self, tag):
+        self.resetflag()
+        print("Encountered an end tag :", tag)
+
+    def handle_data(self, data):
+        # Si el flag de lo que me interesa esta, hacer algo
+        if self.flag == True:
+            m = re.search('.doc', data) #'(?<=abc)def'
+            if m.__len__()>=1:#any(m.group(0)):
+                print("Encontre algo util senpai.............")
+        else:
+            print("Encountered some data  :", data)
+#TODO: Mejorar el manejo de los eventos con otro objeto
+#TODO: Usar doc de exp reg.
+
+# instantiate the parser and fed it some HTML
+parser = MyHTMLParser()
+
+response = request.urlopen('http://python.org/')
+pagina = response.read()
+
+parser.feed(pagina.decode('utf-8'))
+#programita
+
+parser.close()
